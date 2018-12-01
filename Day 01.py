024cb5fa755e9e0a2db302a02280745fa36e4e84
unknown <sreldr03@louisville.edu>
2018-12-01 00:19:03 -0500

    Day 1 finished

--- Day 01.py
+++ Day 01.py
@@ -1,5 +1,20 @@
 # Day 1
 INPUT = open('Day 01.txt', 'r')
+freq = 0
+v = []
 for line in INPUT:
-    pass
+    freq += int(line)
+    v.append(int(line))
+print("part_1:", freq)
+freq = 0
+a = {0}
+i = 0
+while True:
+    freq += v[i]
+    if freq in a:
+        break
+    a.add(freq)
+    i += 1
+    i %= len(v)
+print("part_2:", freq)
 INPUT.close()

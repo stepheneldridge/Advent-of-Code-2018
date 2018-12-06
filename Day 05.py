4355fc7492304616b45d187826d90d6fbebeb21d
unknown <sreldr03@louisville.edu>
2018-12-05 20:51:07 -0500

    Sped up Day 5

--- Day 05.py
+++ Day 05.py
@@ -1,36 +1,39 @@
 # Day 5
 INPUT = open('Day 05.txt', 'r')
 text = INPUT.read().strip()
-data = list(text)
+data = list(map(ord, text))
 
 
 def get_len(data):
+    i = 0
+    popped = False
     while True:
-        remove = []
-        for i in range(len(data) - 1):
-            if i in remove:
-                continue
-            if data[i].upper() == data[i + 1].upper() and data[i] != data[i + 1]:
-                remove.append(i)
-                remove.append(i + 1)
-        remove.reverse()
-        for i in remove:
+        if data[i] ^ data[i + 1] == 32:
+            data.pop(i + 1)
             data.pop(i)
-        if len(remove) == 0:
-            break
+            popped = True
+        else:
+            i += 1
+        if i == len(data) - 1:
+            if popped:
+                i = 0
+                popped = False
+            else:
+                break
     return len(data)
 
 
 def remove_letter(data, letter):
     while letter in data:
         data.remove(letter)
-    while letter.upper() in data:
-        data.remove(letter.upper())
+    while (letter + 32) in data:
+        data.remove(letter + 32)
     return data
 
 
 print("part_1:", get_len(data.copy()))
-letters = set(text.lower())
+
+letters = set(map(ord, text.upper()))
 lengths = []
 for i in letters:
     lengths.append(get_len(remove_letter(data.copy(), i)))

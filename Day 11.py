051fe80d97ddb2ba3915a538316bac7b18b9decc
unknown <sreldr03@louisville.edu>
2018-12-11 02:33:23 -0500

    Rectangles make squares

--- Day 11.py
+++ Day 11.py
@@ -1,6 +1,5 @@
 # Day 11
 INPUT = 2187
-values = {}
 batteries = [[0 for _ in range(300)] for _ in range(300)]
 
 
@@ -14,6 +13,11 @@ def get(x, y, sid):
 for x in range(300):
     for y in range(300):
         batteries[x][y] = get(x, y, INPUT)
+
+sums = [[0 for _ in range(301)] for _ in range(301)]
+for x in range(1, 301):
+    for y in range(1, 301):
+        sums[x][y] = batteries[x - 1][y - 1] + sums[x][y - 1] + sums[x - 1][y] - sums[x - 1][y - 1]
 
 
 def max_for_size(size):
@@ -46,12 +50,25 @@ def max_for_coord(x, y):
     return "%s,%s,%s" % (x, y, size), best_value
 
 
+def max_for_coord_sums(x, y):
+    sizes = min(300 - x, 300 - y)
+    size = 0
+    value = 0
+    best_value = 0
+    for s in range(1, sizes + 1):
+        value = sums[x + s][y + s] - sums[x][y + s] - sums[x + s][y] + sums[x][y]
+        if value > best_value:
+            best_value = value
+            size = s
+    return "%s,%s,%s" % (x, y, size), best_value
+
+
 print("part_1:", max_for_size(3)[0][:-2])
 value = 0
 key = ""
 for x in range(300):
     for y in range(300):
-        r = max_for_coord(x, y)
+        r = max_for_coord_sums(x, y)
         if r[1] > value:
             value = r[1]
             key = r[0]

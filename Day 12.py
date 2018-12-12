b3c2f614b82ee482810050342746e124cdd54e91
unknown <sreldr03@louisville.edu>
2018-12-12 17:23:12 -0500

    Faster but messier

--- Day 12.py
+++ Day 12.py
@@ -6,52 +6,54 @@ start = None
 for line in INPUT:
     m = re.findall(r"[\.#]+", line.strip())
     if start is None:
-        j = 0
-        start = {}
-        for i in m[0]:
-            start[j] = i
-            j += 1
+        start = [True if x == '#' else False for x in m[0]]
     elif len(m) > 0:
         data.append(m)
-rules = {}
+rules = [False for _ in range(2 ** 5)]
 for i in data:
-    rules[i[0]] = i[1]
-
-totals = []
-while len(totals) < 200:
-    k = start.keys()
-    start[max(k) + 1] = '.'
-    start[max(k) + 1] = '.'
-    start[min(k) - 1] = '.'
-    start[min(k) - 1] = '.'
-    new_start = {}
-    for i in start:
-        key = ""
-        if (i - 2) in start:
-            key += start[i - 2]
+    key = 0
+    for j in i[0]:
+        key = key << 1
+        if j == '#':
+            key += 1
+    rules[key] = True if i[1] == '#' else False
+zero = 0
+key = 0
+gen = 0
+total = 0
+diff = 0
+while True:
+    gen += 1
+    key = 0
+    first = False
+    offset = 0
+    empty = True
+    total_t = 0
+    for i in range(len(start)):
+        key = ((key << 1) + start[i]) & 0b011111
+        if empty and zero - 2 + offset + i >= 0 and zero - 2 + offset + i <= 100:
+            empty = not rules[key]
+        if not first and not rules[key]:
+            offset += 1
         else:
-            key += '.'
-        if (i - 1) in start:
-            key += start[i - 1]
+            first = True
+            if rules[key]:
+                total_t += i + zero - 2
+            start[i - offset] = rules[key]
+    zero += offset - 2
+    for i in range(4):
+        key = (key << 1) & 0b011111
+        if rules[key]:
+            total_t += i + zero + len(start)
+        if offset > i:
+            start[i - offset] = rules[key]
         else:
-            key += '.'
-        key += start[i]
-        if (i + 1) in start:
-            key += start[i + 1]
-        else:
-            key += '.'
-        if (i + 2) in start:
-            key += start[i + 2]
-        else:
-            key += '.'
-        new_start[i] = rules[key]
-    start = new_start
-    total = 0
-    for i in start:
-        if start[i] == '#':
-            total += i
-    totals.append(total)
-print("part_1:", totals[19])
-p2 = (50000000000 - len(totals)) * (totals[-1] - totals[-2]) + totals[-1]
-print("part_2:", p2)
+            start.append(rules[key])
+    diff = total_t - total
+    total = total_t
+    if gen == 20:
+        print("part_1:", total)
+    if empty is True:
+        break
+print("part_2:", (int(5e10) - gen) * diff + total)
 INPUT.close()
